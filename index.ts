102aa5e6ea56973340b4ced35169c5f93a1bc623
akhil-haridas <4khilharidas@gmail.com>
2024-01-01 20:48:32 +0530

    Defined Types

--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-//Infer Types
+//Infer Types (Implicit Types)
 
 let userName = "akhil"
+
+//Defining Types (Explicit Types)
+
+let Name: string = "akhil"
+let Age: number = 22
+let single: boolean = true
+let skills: string[] = ["JS", "TS"]
+let Mobile: number[] = [8, 0, 7, 5, 4, 4, 3, 1, 8, 6]
+let empty: [] = []
+let Address: { House_No: number; Place: string; } = { House_No: 127, Place: "Kerala" }
+
+

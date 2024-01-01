36d28eb18bad93323a62d76a8507476aa07482ab
akhil-haridas <4khilharidas@gmail.com>
2024-01-01 20:50:19 +0530

    Run command and js file

---
+++ index.js
@@ -0,0 +1,10 @@
+//Infer Types (Implicit Types)
+var userName = "akhil";
+//Defining Types (Explicit Types)
+var Name = "akhil";
+var Age = 22;
+var single = true;
+var skills = ["JS", "TS"];
+var Mobile = [8, 0, 7, 5, 4, 4, 3, 1, 8, 6];
+var empty = [];
+var Address = { House_No: 127, Place: "Kerala" };

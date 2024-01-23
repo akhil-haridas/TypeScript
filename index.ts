731fcadeb701fbfd75fa8df1d740335a9b6a690e
akhil-haridas <4khilharidas@gmail.com>
2024-01-23 22:03:49 +0530

    Revert "Void"
    This reverts commit b47d2d9f48f0951d3728dde0e983374b4cc606a0.

--- index.ts
+++ index.ts
@@ -289,10 +289,3 @@ const newValue = nameValue as string
 function throwError(message: string): never{
     throw new Error(message)
 }
-
-
-//Void
-
-function throwError2(message: string): void{
-    console.log(message)
-}
